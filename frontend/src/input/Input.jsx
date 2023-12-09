cecba7c003a5e7dbe456e4bb124d45872a6e7fc4
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-12-10 00:09:31 +0500

    first commit

--- frontend/src/input/Input.jsx
+++ frontend/src/input/Input.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 
 const Input = () => {
     const [inputValue, setInputValue] = useState('');
@@ -13,29 +15,44 @@ const Input = () => {
         setInputValue('');
        }
     }
-    
+    // update tasks
+    const editTask = ()=>{
+
+    }
+    // delete tasks
     const deleteTask = ()=>{
           const updateTasks =  tasks.slice(0,-1);
           settasks(updateTasks);
     }
 
   return (
-    <div>
+    <div className={styles.container}>
+        <div className="input-container">
         <input type="text"  
             value={inputValue}
                 onChange={(e)=> setInputValue(e.target.value)}
         />
+        
         <button onClick={addTask}>add Tasks</button>
+        </div>
         <h1>Tasks</h1>
-        <ul>
+     {
+        tasks.length === 0 ? (
+            <h3>No tasks saved</h3>
+        ): (
+            <ul>
             {tasks.map((task,index)=>(
-                     <div key={index}>
+                     <div key={index} className={styles.taskwrapper}>
                      <li>{task}</li>
-                     <button onClick={deleteTask}>delete Task</button>
+                     <FaEdit onClick={editTask} />
+                     <FaTrash onClick={deleteTask} />
+                     
 
                      </div>
             ))   }
         </ul>
+        )
+     }
     </div>
   )
 }
