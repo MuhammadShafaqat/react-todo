a4dd1387b9dbeea149bcf4ca6f3a6b1528cac9f9
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-12-17 20:52:05 +0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Input from './input/Input'
 
 function App() {
--- frontend/src/input/Input.jsx
+++ frontend/src/input/Input.jsx
@@ -1,60 +1,120 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FaTrash, FaEdit } from 'react-icons/fa';
+import styles from './styles.module.css';
 
 const Input = () => {
-// Post request code 
-const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [data, setData] = useState([]);
+  const [editItemId, setEditItemId] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const addTask = () => {
-  // Perform POST request using Axios
-  axios.post('http://localhost:3000/postItem', { title: inputValue })
-    .then(response => {
-      // Handle success, if needed
-      console.log('POST request successful:', response.data); 
-      setInputValue('');
-    })
-    .catch(error => {
-      // Handle error, if needed
-      console.error('Error sending POST request:', error);
-    });
-};
-
-  //Get Request code
-  const [data, setData] = useState([]); // Initialize data as an empty array
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:3000/getItem');
       setData(response.data);
+      setLoading(false);
     } catch (error) {
-      // Handle any errors from the API request
       console.error('Error fetching data:', error);
     }
   };
+
+  const handleAddOrUpdate = () => {
+    if (editItemId) {
+      // Update task
+      axios.put(`http://localhost:3000/updateItem/${editItemId}`, { title: inputValue })
+        .then(response => {
+          console.log('PUT request successful:', response.data);
+          setEditItemId(null);
+          setInputValue('');
+          fetchData();
+        })
+        .catch(error => {
+          console.error('Error updating item:', error);
+        });
+    } else {
+      // Add new task
+      axios.post('http://localhost:3000/postItem', { title: inputValue })
+        .then(response => {
+          console.log('POST request successful:', response.data);
+          setInputValue('');
+          fetchData();
+        })
+        .catch(error => {
+          console.error('Error sending POST request:', error);
+        });
+    }
+  };
+
+  const handleEdit = (itemId, currentTitle) => {
+    setEditItemId(itemId);
+    setInputValue(currentTitle);
+  };
+
+  const handleDelete = (itemId) => {
+    axios.delete(`http://localhost:3000/deleteItem/${itemId}`)
+      .then(response => {
+        console.log('DELETE request successful:', response.data);
+        fetchData();
+      })
+      .catch(error => {
+        console.error('Error deleting item:', error);
+      });
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
-    <div>
-  <h3>Tasks created</h3>
+    <div className={styles.container}>
+     <div  className={styles.subContainer}>
+     <h3>create Tasks</h3>
       <div className="input-wrapper">
-        <input type="text" 
+        <input
+          type="text"
+          placeholder='what is the task today?'
           value={inputValue}
-          onChange={(e)=> setInputValue(e.target.value)}
+          onChange={(e)=> setInputValue(e.target.value)} className={styles.dataInput}
         />
-        <button  onClick={addTask}>Task</button>
+        <button onClick={handleAddOrUpdate} type='button' className= {styles.button1} >{editItemId ? 'Save' : 'Add Task'}</button>
       </div>
-  
 
-
-
-    <br /><br />
-           <h3>Here is the comming data from the Api</h3>
-      {data && data.map((item, index) => (
-        <div key={index}>{item.title}</div>
-      ))}
-      
+      <br /><br />
+      <h3>My Tasks</h3>
+      {loading ? (
+        <p>Loading...</p>
+      ) : data && data.length === 0 ? (
+        <p>No data</p>
+      ) : (
+        data.map((item, index) => (
+          <div key={item._id} style={{ display: 'flex', alignItems: 'center' }}>
+            {editItemId === item._id ? (
+              <>
+                {/* No additional input or button */}
+              </>
+            ) : (
+              <>
+          <div className={styles.tasks}>
+          <span className='h4'>{index}.</span>
+                <p style={{ marginLeft: '5px', fontSize: '20px' }} className={styles.title}>{item.title}</p>
+                <FaEdit
+                  style={{ fontSize: '18px', color: 'blue', marginLeft: '10px', cursor: 'pointer' }}
+                  onClick={() => handleEdit(item._id, item.title)}
+                />
+                <FaTrash
+                className='mr-1'
+                  style={{ fontSize: '18px', color: 'red', marginLeft: '10px', cursor: 'pointer' }}
+                  onClick={() => handleDelete(item._id)}
+                />
+          </div>
+              </>
+            )}
+          </div>
+        ))
+      )}
+     </div>
     </div>
   );
 };
